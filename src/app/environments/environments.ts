e54261c4d3d0fadbc18f1066528141d6b52b2f1e
alinenink <116383618+alinenink@users.noreply.github.com>
2025-07-20 21:39:41 -0300

    deploy

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -2,6 +2,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://taverna-backend.onrender.com/api',
-   apiUrl: 'http://localhost:3001/api'
-  // apiUrl: 'https://taverna-backend-vc-rrk3.vercel.app/api',
+  //  apiUrl: 'http://localhost:3001/api'
+  apiUrl: 'https://taverna-backend-vc-rrk3.vercel.app/api',
 };
