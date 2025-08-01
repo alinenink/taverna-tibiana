043bd6a10d905a3936524c10cda78f2718e9ff66
alinenink <116383618+alinenink@users.noreply.github.com>
2025-08-01 08:44:31 -0300

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit, signal, computed } from '@angular/core';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { AnalyticsService } from './services/analytics.service';
 import { BeerCtaComponent } from './components/beer-cta/beer-cta.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, BeerCtaComponent],
+  imports: [RouterModule, BeerCtaComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'tavernatibiana';
 
-  constructor(private analyticsService: AnalyticsService) {}
+  private readonly currentRoute = signal<string>('');
+
+  readonly showBeerCta = computed(() => {
+    const route = this.currentRoute();
+    return route !== '' && route !== 'login';
+  });
+
+  constructor(
+    private analyticsService: AnalyticsService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     // Track app initialization
     this.analyticsService.trackEvent('app_initialized', {
       app_name: 'Taverna Tibiana',
     });
+
+    // Monitor route changes to show/hide beer CTA
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const url = event.urlAfterRedirects;
+        const route = url === '/' ? '' : url.replace('/', '');
+        this.currentRoute.set(route);
+      });
   }
 }
